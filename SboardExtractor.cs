e611e07770d640d4ad0d1999a194ca015787db40
c-closed <c-closed@naver.com>
2026-07-14 07:40:30 +0900

    v1.4.7.5: remove KillSboard from btnClose (keep Sboard running on exit)

--- SboardExtractor.cs
+++ SboardExtractor.cs
@@ -14,8 +14,8 @@ using AutoUpdaterDotNET;
 [assembly: System.Reflection.AssemblyProduct("Sboard 추출기")]
 [assembly: System.Reflection.AssemblyCompany("")]
 [assembly: System.Reflection.AssemblyCopyright("")]
-[assembly: System.Reflection.AssemblyVersion("1.4.7.4")]
-[assembly: System.Reflection.AssemblyFileVersion("1.4.7.4")]
+[assembly: System.Reflection.AssemblyVersion("1.4.7.5")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.7.5")]
 
 namespace SboardExtractor
 {
@@ -167,7 +167,7 @@ namespace SboardExtractor
         private static bool? _excelAvailable;
         private const string LoginWindowTitle = "Sboard";
         private const string SessionPrefix = "Sboard [";
-        private const string AppVersion = "1.4.7.4";
+        private const string AppVersion = "1.4.7.5";
         private const string UpdateXmlUrl = "https://extractor-api.sboard-auto-login.workers.dev/api/update.xml";
         private static ManualResetEvent _extractPause = new ManualResetEvent(true);
         private const byte VK_UP = 0x26;
@@ -719,7 +719,6 @@ namespace SboardExtractor
                 };
                 btnClose.Click += (s, e) =>
                 {
-                    try { KillSboard(); } catch { }
                     try { _extractPause.Set(); } catch { }
                     Close();
                     Application.Exit();
