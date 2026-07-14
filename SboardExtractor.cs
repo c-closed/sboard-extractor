4ec26e40448ea08cbaa9941f28f8889078c1240b
c-closed <c-closed@naver.com>
2026-07-14 16:50:06 +0900

    bump to v1.4.7.7

--- SboardExtractor.cs
+++ SboardExtractor.cs
@@ -14,8 +14,8 @@ using AutoUpdaterDotNET;
 [assembly: System.Reflection.AssemblyProduct("Sboard 추출기")]
 [assembly: System.Reflection.AssemblyCompany("")]
 [assembly: System.Reflection.AssemblyCopyright("")]
-[assembly: System.Reflection.AssemblyVersion("1.4.7.6")]
-[assembly: System.Reflection.AssemblyFileVersion("1.4.7.6")]
+[assembly: System.Reflection.AssemblyVersion("1.4.7.7")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.7.7")]
 
 namespace SboardExtractor
 {
@@ -167,7 +167,7 @@ namespace SboardExtractor
         private static bool? _excelAvailable;
         private const string LoginWindowTitle = "Sboard";
         private const string SessionPrefix = "Sboard [";
-        private const string AppVersion = "1.4.7.6";
+        private const string AppVersion = "1.4.7.7";
         private const string UpdateXmlUrl = "https://extractor-api.sboard-auto-login.workers.dev/api/update.xml";
         private static ManualResetEvent _extractPause = new ManualResetEvent(true);
         private static volatile bool _cancelRequested;
